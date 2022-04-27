0d47a06f9987dcedf9b0d59d2275a8a44d2615cb
Ivan <75050033+Milka127@users.noreply.github.com>
2022-04-27 12:36:33 +0300

    Create reg.php

---
+++ reg.php
@@ -0,0 +1,7 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+
+echo "Имя: $name<br> Фамилия: $lastname<br> Email: $email<br> Пароль: $pass";
