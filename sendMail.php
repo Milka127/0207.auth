9f3f401910cebedc4ece88e43c85e9ebd5c5a107
Ivan <75050033+Milka127@users.noreply.github.com>
2022-04-27 12:37:17 +0300

    Create sendMail.php

---
+++ sendMail.php
@@ -0,0 +1,9 @@
+<?php
+// $userName = $_GET['user_name'];
+$userName = $_POST['user_name'];
+
+// mail('join25758@gmail.com', 'Текстовое письмо', "Привет, $userName");
+
+echo "Hello! $userName";
+
+?>
