121ee3cd3e73286868ad037fe1fe0ff1a6bba4c2
Ivan <75050033+Milka127@users.noreply.github.com>
2022-04-30 14:03:04 +0300

    Delete sendMail.php

--- sendMail.php
+++
@@ -1,9 +0,0 @@
-<?php
-// $userName = $_GET['user_name'];
-$userName = $_POST['user_name'];
-
-// mail('join25758@gmail.com', 'Текстовое письмо', "Привет, $userName");
-
-echo "Hello! $userName";
-
-?>
